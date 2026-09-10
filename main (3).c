5754b91c98cbb0124ca4499b2731fdcef11f5246
Willian-jesus19 <97willian.jesus@gmail.com>
2026-09-10 11:16:54 -0300

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h> 
+int main()
+{
+    
+   
+    int A = 5;
+    int B = 10;
+    int temp;
+    
+    printf ("Antes da troca: A = %d, B = %d\n", A, B);
+    
+    temp = A;
+    A = B;
+    B = temp;
+    
+    printf("Após a troca: A = %d, B = %d", A, B);
+    
+    
+       
+}
+
+////////////////////////////////////
+
+
+    
